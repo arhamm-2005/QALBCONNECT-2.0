6b86ffe7bd4b203e3ab075bf622e4235bcb33ecf
arham <arhamshakil70@gmail.com>
2025-06-14 01:40:46 +0500

    project initialization

---
+++ qalbconnect-backend/src/main/java/com/qalbconnect/qalbconnect_backend/QalbconnectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.qalbconnect.qalbconnect_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QalbconnectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QalbconnectBackendApplication.class, args);
+	}
+
+}
---
+++ qalbconnect-backend/src/test/java/com/qalbconnect/qalbconnect_backend/QalbconnectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qalbconnect.qalbconnect_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QalbconnectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
